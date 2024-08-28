ad9b973e270c1b1fc46a8ec2bb672713327bc29b
yossef <yossefmagdy78@gmail.com>
2024-08-28 14:05:07 +0300

    add slug column to posts table

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,13 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    use Sluggable;
     protected $fillable = ["title", "description", "post_creator_id", "image"];
     function postCreators() {
         return $this->BelongsTo(PostCreator::class, "post_creator_id", "id");
+    }
+    public function sluggable(): array {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
